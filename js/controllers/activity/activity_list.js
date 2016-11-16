4984c1a9188f2f241c6062002e2f3ae20143eaf4
Spider00153 <spider00153@hotmail.com>
2016-11-16 23:10:19 +0800

    1

--- js/controllers/activity/activity_list.js
+++ js/controllers/activity/activity_list.js
@@ -19,8 +19,9 @@ function getActivityList() {
 		sfkh: selecttype,
 		nowindex:  starIndex
 	};
+	//alert(JSON.stringify(data))
 	common.postApi('GetScore', data, function(response) {
-		//alert(JSON.stringify(response))
+		
 		dataArray = eval(response.data);
 		
 		for (var i = 0; i < dataArray.length; i++) {
@@ -49,6 +50,7 @@ function getActivityList() {
 		}
 		//alert(starIndex)
 		starIndex = starIndex + 10;
+		
 		mui('#pullrefresh').pullRefresh().endPullupToRefresh((dataArray.length < 10)); //参数为true代表没有更多数据了。
 //		if (selecttype == "N") {
 //
